8f6c35fb993aafbf7fa4031d08438d104cda308b
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2021-10-08 15:33:53 -0400

    Web Page Generator App update
     push for updated web page generator for submission to the Tech Academy

--- WP_Gen/write_html_GUI.py
+++ WP_Gen/write_html_GUI.py
@@ -10,18 +10,18 @@ class ParentWindow(Frame):
 
         #master frame config
         self.master = master
-        self.master.minsize(500,300)
-        self.master.maxsize(500,300)
+        self.master.minsize(700,225)
+        self.master.maxsize(700,225)
         self.master.title("Web Page Generator")
         self.master.configure(bg="#F0F0F0")
         arg = self.master
 
-        #func to get the entry
+        #func to get the entered text
         def body_insert_func():
-            body_insert = self.txt_body.get()
+            body_insert = self.txt_body.get("1.0",'end') #1.0=read from line 1, char 0; end=read until the end
             html_file = open('your_html.html', 'w')
 
-            #set the entry into the html
+            #set the text into the html
             message = """<html>
             <head></head>
             <body><p>{}</p></body>
@@ -36,15 +36,18 @@ class ParentWindow(Frame):
         def open_page():
             webbrowser.open_new_tab('your_html.html')
 
+        #text label
+        self.lbl_body = tk.Label(self.master,text='Enter your text:')
+        self.lbl_body.grid(row=0,column=0,padx=(30,0),pady=(20,0),sticky=N+W)
         #textbox for entry
-        self.txt_body = tk.Entry(self.master,text='Enter your text here')
-        self.txt_body.grid(row=1,column=0,rowspan=4,columnspan=4,padx=(30,30),pady=(20,0),sticky=N+E+W)
+        self.txt_body=tk.Text(self.master,height=4)
+        self.txt_body.grid(row=1,column=0,columnspan=2,padx=(30,30),pady=(20,0),sticky=E+W)
         #the add button
         self.btn_add = tk.Button(self.master,width=12,height=2,text='Add Body',command=body_insert_func)
-        self.btn_add.grid(row=9,column=0,padx=(30,0),pady=(20,10),sticky=N+E+W)
+        self.btn_add.grid(row=2,column=0,padx=(30,0),pady=(20,10),sticky=N+E+W)
         #the button to create the webpage
         self.btn_create = tk.Button(self.master,width=12,height=2,text='Open Page',command=open_page)
-        self.btn_create.grid(row=9,column=4,padx=(30,30),pady=(20,10),sticky=N+E+W)
+        self.btn_create.grid(row=2,column=1,padx=(30,30),pady=(20,10),sticky=N+E+W)
 
         
 
