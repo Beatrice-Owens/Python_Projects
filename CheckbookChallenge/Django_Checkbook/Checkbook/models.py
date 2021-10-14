2daf5345503d9cf2dab92039357704499fbe2d53
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2021-10-13 21:08:36 -0400

    Django Checkbook Revision
    Updating my checkbook challenge project having fixed the issue of transactions and balance not displaying.

--- CheckbookChallenge/Django_Checkbook/Checkbook/models.py
+++ CheckbookChallenge/Django_Checkbook/Checkbook/models.py
@@ -11,7 +11,7 @@ class Account(models.Model):
     # allows references to a specific account to be returned
     # as the owner's name not the primary key
     def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+        return self.first_name + ' ' + self.last_name
 
 
 # transaction model
